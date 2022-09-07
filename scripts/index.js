4b8f13bff9a2679b1a8e4cae693a7b24fd9d9518
Petr Voevodin <demonle01@yandex.ru>
2022-09-07 07:16:14 +0300

    проектная 4.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const popup = document.querySelector(".popup");
-const openEditButton = document.querySelector(".profile__edit-button");
-const closePopupButton = document.querySelector(".button_close");
+const openEditButton = document.querySelector(".button_type_edit");
+const closePopupButton = document.querySelector(".button_type_close");
 const formElement = document.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__input_name");
 const jobInput = document.querySelector(".popup__input_job");
@@ -8,14 +8,14 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 
 openEditButton.addEventListener("click", () => {
-  popup.classList.add("popup_edit-button");
+  popup.classList.add("popup_is-opened");
 
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
 
 closePopupButton.addEventListener("click", () => {
-  popup.classList.remove("popup_edit-button");
+  popup.classList.remove("popup_is-opened");
 
 });
 
@@ -24,16 +24,16 @@ function formSubmitHandler(evt) {
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove("popup_edit-button");
+  popup.classList.remove("popup_is-opened");
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
 
 const cards = document.querySelectorAll(".cards");
-const buttonsLike = document.querySelectorAll(".cards__like");
+const buttonsLike = document.querySelectorAll(".button_type_like");
 console.log(buttonsLike);
 buttonsLike.forEach((button) => {
   button.addEventListener("click", () => {
-    button.classList.toggle("cards__like_active");
+    button.classList.toggle("button_active");
   });
 });
